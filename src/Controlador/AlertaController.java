007d5d9f208597387fb55eb9a83466a505ef1e0d
Camilo Rodriguez <camilojoserodriguezgarcia@gmail.com>
2026-05-07 23:14:31 -0500

    Logica Configuracion Usuario

--- src/Controlador/AlertaController.java
+++ src/Controlador/AlertaController.java
@@ -10,6 +10,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 import Controlador.PantallaSignUpController;
+import java.io.IOException;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -25,18 +33,41 @@ public class AlertaController implements Initializable {
     @FXML
     private Text mensaje;
     PantallaSignUpController control;
+    String accion;
+    @FXML
+    private Pane Overlay;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
-    public void cargarDatos(String mensaje, PantallaSignUpController control){
-        this.control = control;
+    public void cargarDatos(String mensaje, String accion){
+        
         this.mensaje.setText(mensaje);
+        this.accion = accion;
     }
     
-    public void cerrar(){
-        control.cerrar();
+    public void setOverlay(Pane Overlay){
+        this.Overlay = Overlay;
+    }
+    
+    public void cerrar(Event event){
+        if(accion == "cerrar"){
+            Overlay.setVisible(false);
+        }
+        
+        else{
+            try{
+        Parent root = FXMLLoader.load(getClass().getResource(accion));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = stage.getScene();
+        scene.setRoot(root);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+    }
+        }
+        
     }
 }
